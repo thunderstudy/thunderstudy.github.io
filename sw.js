606dfed7901f703ff877431b426c8f212c1ed335
thunderstudy <thunderofficialx@gmail.com>
2026-05-22 11:28:01 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,9 +5,9 @@
  * @author Wondermayank / ThunderStudy
  */
 
-const CACHE_NAME = 'thunderstudy-v3';
-const STATIC_CACHE = 'thunderstudy-static-v3';
-const DYNAMIC_CACHE = 'thunderstudy-dynamic-v3';
+const CACHE_NAME = 'thunderstudy-v4';
+const STATIC_CACHE = 'thunderstudy-static-v4';
+const DYNAMIC_CACHE = 'thunderstudy-dynamic-v4';
 
 // Core assets to cache on install
 const CORE_ASSETS = [
